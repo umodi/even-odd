6d06db11af9db676738f415a0cd5169dbddbf0ac
iamudit <umodi318@gmail.com>
2022-05-18 22:23:56 +0530

    grammar correction

--- evenodd.cpp
+++ evenodd.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int a;
-    cout<<"Enter a whole number : ";
-    cin>>a;
+    cout << "Enter a whole number : ";
+    cin >> a;
     int remainder = a % 2;
 
-    if(remainder==0)
+    if (remainder == 0)
     {
-        cout<<"The entered number is even\n";
+        cout << "The entered number is a even\n";
     }
     else
     {
-        cout<<"The entered number is odd\n";
+        cout << "The entered number is a odd\n";
     }
 
-    cout<<"Thank You\n";
+    cout << "Thank You\n";
 
     return 0;
     system("pause>0");
